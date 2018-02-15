2951ed9ac4d0978a735188a878c28672e18a36f2
Kateryna Zhuzha <KZhuzha@suitsupply.com>
2018-02-15 08:45:42 +0100

    add debug on error for behave

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 
 def before_all(context):
@@ -7,6 +8,23 @@ def before_all(context):
     context.browser.implicitly_wait(10)
     context.browser.maximize_window()
 
+    setup_debug_on_error(context.config.userdata)
+
 
 def after_all(context):
     context.browser.quit()
+
+BEHAVE_DEBUG_ON_ERROR = False
+
+
+def setup_debug_on_error(userdata):
+    global BEHAVE_DEBUG_ON_ERROR
+    BEHAVE_DEBUG_ON_ERROR = userdata.getbool("BEHAVE_DEBUG_ON_ERROR")
+
+
+def after_step(context, step):
+    if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
+        # -- ENTER DEBUGGER: Zoom in on failure location.
+        # NOTE: Use IPython debugger, same for pdb (basic python debugger).
+        import ipdb
+        ipdb.post_mortem(step.exc_traceback)
