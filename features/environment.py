b28938553d5569a862c7530e9f87883fb5e6a247
Kateryna Zhuzha <kathrin.krylova@gmail.com>
2018-04-02 23:03:51 +0200

    remove explicit path to chromedriver

--- features/environment.py
+++ features/environment.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 
 def before_all(context):
-    context.browser = webdriver.Chrome(executable_path="/Users/kzhuzha/work/python/chromedriver")
+    context.browser = webdriver.Chrome()
     context.browser.set_page_load_timeout(10)
     context.browser.implicitly_wait(10)
     context.browser.maximize_window()
