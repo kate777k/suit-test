121fa37226b85a6da83476fb3582613d126c81fc
Kateryna Zhuzha <kathrin.krylova@gmail.com>
2018-04-02 23:03:36 +0200

    remove debug on error

--- features/environment.py
+++ features/environment.py
@@ -1,33 +1,18 @@
-from selenium import webdriver
 import time
+
+from selenium import webdriver
 
 
 def before_all(context):
     context.browser = webdriver.Chrome(executable_path="/Users/kzhuzha/work/python/chromedriver")
-    # context.browser = webdriver.Firefox(executable_path="/Users/kzhuzha/work/python/geckodriver")
     context.browser.set_page_load_timeout(10)
     context.browser.implicitly_wait(10)
     context.browser.maximize_window()
-
-    setup_debug_on_error(context.config.userdata)
 
 
 def after_all(context):
     context.browser.quit()
 
 
-BEHAVE_DEBUG_ON_ERROR = False
-
-
-def setup_debug_on_error(userdata):
-    global BEHAVE_DEBUG_ON_ERROR
-    BEHAVE_DEBUG_ON_ERROR = userdata.getbool("BEHAVE_DEBUG_ON_ERROR")
-
-
 def after_step(context, step):
-    if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
-        # -- ENTER DEBUGGER: Zoom in on failure location.
-        # NOTE: Use IPython debugger, same for pdb (basic python debugger).
-        import ipdb
-        ipdb.post_mortem(step.exc_traceback)
     time.sleep(2)
